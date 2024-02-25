cbc82acfa5f4792cd6faa95840b42b8f92ce4732
Sandun Munasinghe <reachsandun@gmail.com>
2024-02-25 18:08:50 +0530

    added NRF trasmitter message code to broadcast the car availability in the gragage

--- Car_in_garage_detector1/Car_in_garage_detector1.ino
+++ Car_in_garage_detector1/Car_in_garage_detector1.ino
@@ -1,16 +1,31 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
 const int trigPin = 9;  
 const int echoPin = 10; 
 const int carDistanceThresholdCm = 30;
 
 float duration, distance;  
 
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+
+void broadcastMessage(String message){
+  Serial.println(message); 
+  radio.write(&message, sizeof(message));
+  Serial.println("Radio message sent!"); 
+}
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(trigPin, OUTPUT);  
 	pinMode(echoPin, INPUT);  
 	Serial.begin(9600);  
-
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
 }
 
 void loop() {
@@ -24,12 +39,17 @@ void loop() {
   distance = (duration*.0343)/2;
   Serial.print("Distance: ");  
 	Serial.println(distance);   
+
+  String message;
   if(distance < carDistanceThresholdCm){
     // car is in the garage
-    Serial.println("Car is in the garage :GREEN"); 
+    message = "Car is in the garage :GREEN";
   }else{
     // car is not in the garage
-    Serial.println("Car is not in the garage :RED"); 
+    message = "Car is not in the garage :RED"; 
   }
+
+  broadcastMessage(message);
+
   delay(5000); 
 }
