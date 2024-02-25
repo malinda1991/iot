83342d782fc7d41139f2abd60e7c5c037c278db6
Sandun Munasinghe <reachsandun@gmail.com>
2024-02-26 00:40:43 +0530

    wireless trasmission enhasments

--- Car_in_garage_detector1/Car_in_garage_detector1.ino
+++ Car_in_garage_detector1/Car_in_garage_detector1.ino
@@ -1,27 +1,39 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Arduino_JSON.h>
+#include <math.h>
 
-const int trigPin = 9;  
-const int echoPin = 10; 
-const int carDistanceThresholdCm = 30;
+const int trigPin = 9;
+const int echoPin = 10;
+const int carDistanceCm = 30;
+const int LOOP_DELAY = 5000;
+// const byte GARAGE_RF_CHANNEL[6] = "G1083";
 
-float duration, distance;  
+float duration, distance;
+RF24 radio(7, 8);  // CE, CSN
 
-RF24 radio(7, 8); // CE, CSN
-const byte address[6] = "00001";
+const byte address[6] = "G1083";
+const int rfPayloadBytesLimit = 32;
 
-void broadcastMessage(String message){
-  Serial.println(message); 
+void broadcastMessage(JSONVar dataObj) {
+  String jsonPayload = JSON.stringify(dataObj);
+  char message[jsonPayload.length() + 1];
+
+  for (int x = 0; x < jsonPayload.length() + 1; x++) {
+    message[x] = jsonPayload[x];
+  }
+
   radio.write(&message, sizeof(message));
-  Serial.println("Radio message sent!"); 
+  Serial.println(message);
+  Serial.println("Radio message sent!");
 }
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(trigPin, OUTPUT);  
-	pinMode(echoPin, INPUT);  
-	Serial.begin(9600);  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
@@ -30,26 +42,36 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(trigPin, LOW);  
-	delayMicroseconds(2);  
-	digitalWrite(trigPin, HIGH);  
-	delayMicroseconds(10);  
-	digitalWrite(trigPin, LOW);  
-  duration = pulseIn(echoPin, HIGH);  
-  distance = (duration*.0343)/2;
-  Serial.print("Distance: ");  
-	Serial.println(distance);   
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration * .0343) / 2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
 
   String message;
-  if(distance < carDistanceThresholdCm){
+  bool isCarInGarage = false;
+
+  if (distance < carDistanceCm) {
     // car is in the garage
     message = "Car is in the garage :GREEN";
-  }else{
+    isCarInGarage = true;
+  } else {
     // car is not in the garage
-    message = "Car is not in the garage :RED"; 
+    message = "Car is not in the garage :RED";
   }
 
-  broadcastMessage(message);
+  JSONVar dataObj;
 
-  delay(5000); 
+  dataObj["dist"] = distance;
+  dataObj["car"] = isCarInGarage;
+  dataObj["door"] = true;
+
+  broadcastMessage(dataObj);
+  Serial.println(message);
+  Serial.println("-------------");
+  delay(LOOP_DELAY);
 }
