7b40e6127369b49e663c953c91c8851817fb4f83
Sandun Munasinghe <reachsandun@gmail.com>
2024-06-13 17:47:31 +0530

    changes for the receiver board code and temporary comments

--- iot_wireless_data_receiver/iot_wireless_data_receiver.ino
+++ iot_wireless_data_receiver/iot_wireless_data_receiver.ino
@@ -1,11 +1,9 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-// #include <Arduino_JSON.h>
 
 RF24 radio(7, 8); // CE, CSN
-// const GARAGE_RF_CHANNEL = "";
-const byte address[6] = "G1083";
+const byte GARAGE_RF_CHANNEL[6] = "G1083";
 // The number of bytes that be sent through NRF24 at one time to one direction
 const int nrfPayloadLimit = 32;
 
@@ -13,7 +11,7 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   radio.begin();
-  radio.openReadingPipe(0, address);
+  radio.openReadingPipe(0, GARAGE_RF_CHANNEL);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 }
